b7980fff0cfd389f1e3aaaec4dd2c3b817b24d1b
Mohit <mohitsingh@outlook.in>
2017-11-26 19:57:01 +0530

    test second repository

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("This is my first git example")
+        print("This is my second repository")
+        
     }
 
     override func didReceiveMemoryWarning() {
